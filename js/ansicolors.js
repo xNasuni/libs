2bae59f98c0d529f202ebc7d8e709576e72a667e
xNasuni <62818119+xNasuni@users.noreply.github.com>
2021-10-27 17:02:12 -0400

    add library : ansicolors.js
    ansicolors - added

---
+++ js/ansicolors.js
@@ -0,0 +1,18 @@
+/*
+  -- XNASUNIS LIBS/JS --
+  
+  -- ANSI COLOR LIB --
+  
+  -- UNDER GNU 3.0 LICENSE FEEL FREE TO USE --
+  
+  -- CONTACT FOR HELP: xNasuni#8498 --
+  
+*/
+
+/*
+
+  WORK IN PROGRESS
+  
+  PORTING LUA COLOR LIB TO JS
+  
+*/
